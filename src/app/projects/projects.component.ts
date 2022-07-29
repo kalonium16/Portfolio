bec93df62e1de0071d6ea41176c954655aa7a6dc
Anton Latanskyy <52884811+kalonium16@users.noreply.github.com>
2022-07-29 16:35:52 +0300

    prijects update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images2 = [
+    '../../assets/weather1.png',
+    '../../assets/weather2.png',
+    '../../assets/weather3.png',
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
